6bcbf10d53858f6f2e4dea57f363359e3782f206
cfinn68916 <56322557+cfinn68916@users.noreply.github.com>
2021-06-29 09:35:44 -0500

    Update web-app.js

--- web-app.js
+++ web-app.js
@@ -1,9 +1,22 @@
-(function () {
-
-    // use strict enforces more rules
-    // rules make us better programmers
+function init () {
     "use strict";
-    alert("hello");
-
-
-}());
+    function ById(em){
+        return document.getElementById(em)
+    }
+    let n=0;
+    function subm(){
+        let v=ById("add").value;
+        const para=document.createElement("li");
+        const node=document.createTextNode(''+n+':'+v);
+        para.setAttribute("id",''+n);
+        para.appendChild(node);
+        ById("div1").appendChild(para);
+        n=n+1;
+    }
+    function del(){
+        v=ById("removeid").value
+        ById(v).remove();
+    }
+    ById("sm").addEventListener("click",subm)
+    ById("rm").addEventListener("click",del)
+};
