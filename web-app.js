45027b3bce17d9a19aa6e8598701751f84973abe
cfinn68916 <56322557+cfinn68916@users.noreply.github.com>
2021-07-01 10:41:41 -0500

    Update web-app.js

--- web-app.js
+++ web-app.js
@@ -1,8 +1,10 @@
 function init () {
     "use strict";
+    let used=[];
     function ById(em){
         return document.getElementById(em)
     }
+    
     let n=0;
     function subm(){
         let v=ById("add").value;
@@ -10,13 +12,32 @@ function init () {
         const node=document.createTextNode(''+n+':'+v);
         para.setAttribute("id",''+n);
         para.appendChild(node);
+        
+        
+        const db=document.createElement("button");
+        const bn=document.createTextNode('delete');
+        db.appendChild(bn);
+        db.setAttribute("id",'b'+n)
+        para.appendChild(db);
         ById("div1").appendChild(para);
+        used.push(n)
         n=n+1;
     }
+    function del_by_item(v){
+        let i=ById(v);
+        if (i!=null){
+            i.remove();
+        }
+    }
     function del(){
-        v=ById("removeid").value
-        ById(v).remove();
+        let v=ById("removeid").value;
+        del_by_item(v);
+    }
+    function del_all(){
+        used.forEach(del_by_item)
+        used=[];
     }
     ById("sm").addEventListener("click",subm)
     ById("rm").addEventListener("click",del)
+    ById("rm_all").addEventListener("click",del_all)
 };
